661e1a6bbf2d03adad09ec62cfa487b3d6b39efe
Paul Chen <@>
2019-12-23 13:24:09 +0200

    Initial commit

---
+++ src/chenP/CustomList.java
@@ -0,0 +1,176 @@
+package chenP;
+
+import java.util.Arrays;
+
+public class CustomList {
+
+    public static int[] arr = new int[0];
+
+    public static int[] getArr() {
+        return arr;
+    }
+
+    public CustomList() {
+
+    }
+
+    public CustomList(int[] arr) {
+        this.arr = arr;
+    }
+
+    public static int get(int index) {
+        try {
+            if (indexValidator(index) >= 0) {
+                return arr[index];
+            } else {
+                indexError();
+                return Integer.parseInt(null);
+            }
+        } catch (Exception e) {
+            System.out.println("get method error");
+            return Integer.parseInt(null);
+        }
+    }
+
+    public static void set(int index, int value) {
+        try {
+            if (indexValidator(index) >= 0) {
+                arr[index] = value;
+            } else {
+                indexError();
+            }
+        } catch (Exception e) {
+            System.out.println("set method error");
+        }
+    }
+
+    public static void add(int value) {
+        try {
+            if (arr.length == 0) {
+                int[] tmp = new int[1];
+                tmp[0] = value;
+                arr = tmp;
+                return;
+            }
+            int[] tmp = new int[arr.length + 1];
+            System.arraycopy(arr, 0, tmp, 0, arr.length);
+            tmp[tmp.length - 1] = value;
+            arr = tmp;
+        } catch (Exception e) {
+            System.out.println("add method error");
+        }
+    }
+
+    public static void clear() {
+        arr = new int[0];
+    }
+
+    public static int size() {
+        return arr.length;
+    }
+
+    public static boolean isEmpty() {
+        return size() == 0;
+    }
+
+    // опционально
+
+    public static void add(int index, int value) {
+        try {
+            if (indexValidator(index) >= 0) {
+                int[] tmp = new int[arr.length + 1];
+                System.arraycopy(arr, 0, tmp, 0, index);
+                tmp[index] = value;
+                System.arraycopy(arr, index, tmp, index + 1, arr.length - index);
+                arr = tmp;
+            } else {
+                indexError();
+            }
+        } catch (Exception e) {
+            System.out.println("add method error");
+        }
+    }
+
+    public static void remove(int index) {
+        try {
+            if (indexValidator(index) >= 0) {
+                int[] tmp = new int[arr.length - 1];
+                System.arraycopy(arr, 0, tmp, 0, index);
+                System.arraycopy(arr, index + 1, tmp, index, arr.length - index - 1);
+                arr = tmp;
+                return;
+            }
+            System.out.println("Invalid index");
+        } catch (Exception e) {
+            System.out.println("remove method error");
+        }
+    }
+
+    public static void removeByValue(int value) {
+        try {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == value) {
+                    remove(i);
+                    removeByValue(value);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("remove method error");
+        }
+    }
+
+    public static int[] toArray() {
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = arr[i];
+        }
+        return result;
+    }
+
+    public static void sort() {
+        Arrays.sort(arr);
+    }
+
+    private static void indexError() {
+        System.out.println("invalid index");
+    }
+
+    private static int indexValidator(int index) {
+        /**
+         * Если индекс > 0 - return 1
+         * Если индекс == 0 - return 0
+         * Если index out of range return < 0
+         * Если index < 0 - return -1
+         * Если индекс > arr.length - return -2
+         */
+        try {
+            if (index > 0 && index < arr.length) {
+                return 1;
+            }
+            if (index == 0) {
+                return 0;
+            }
+            if (index < 0) {
+                return (-1);
+            }
+            if (index >= arr.length) {
+                return -2;
+            } else {
+                System.out.println("validator error");
+                return Integer.parseInt(null);
+            }
+        } catch (Exception e) {
+            System.out.println("validator error");
+            return Integer.parseInt(null);
+        }
+    }
+
+    public static String string() {
+        return "CustomList{" +
+                "arr=" + Arrays.toString(arr) +
+                '}';
+    }
+}
+
+
+
---
+++ src/chenP/Main.java
@@ -0,0 +1,72 @@
+package chenP;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+        //Initialise list
+        new CustomList();
+
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            //add test
+            CustomList.add(random.nextInt(100));
+            list1.add(random.nextInt(100));
+        }
+        list1.add(10, 666);
+        list1.set(10, 666);
+        printList(list1);
+        list1.set(4, 12);
+        //set test
+        CustomList.set(0, 999);
+        //get test
+        System.out.println(CustomList.get(0));
+        //string test
+        System.out.println(CustomList.string());
+        //size test
+        System.out.println("size " + CustomList.size());
+        //clear test
+        CustomList.clear();
+        System.out.println(CustomList.string());
+        //size test
+        System.out.println("size " + CustomList.size());
+        //isEmpty test
+        System.out.println(CustomList.isEmpty());
+
+        //опциональные
+
+        for (int i = 0; i <= 10; i++) {
+            //add test
+            CustomList.add(i);
+        }
+        System.out.println(CustomList.string());
+        //add (index,value) test
+        CustomList.add(5, 5);
+        System.out.println(CustomList.string());
+        //remove (index,value) test
+        CustomList.remove(0);
+        System.out.println(CustomList.string());
+        //removeByValue test
+        CustomList.removeByValue(5);
+        System.out.println(CustomList.string());
+        //toArray test
+        int[] arr = CustomList.toArray();
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < CustomList.size(); i++) {
+            //add test
+            CustomList.set(i, random.nextInt(10));
+        }
+        //sort test
+        System.out.println(CustomList.string());
+        CustomList.sort();
+        System.out.println(CustomList.string());
+
+    }
+
+    private static void printList(List<Integer> list) {
+        System.out.println(list);
+    }
+}
+
